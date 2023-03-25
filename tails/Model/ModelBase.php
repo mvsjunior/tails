ab6f21e852358742961cd85a5f055b4da9926151
Mauro <mauro.emc@outlook.com>
2023-03-25 00:01:05 -0300

    Modelo base

---
+++ tails/Model/ModelBase.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace VolgPhp\Tails\Model;
+
+use VolgPhp\Tails\Connection;
+
+abstract class ModelBase {
+
+    static $conn;
+    protected $TABLE;
+    protected $className = __CLASS__;
+    protected array $newData  = [];
+    protected $fillable;
+
+    public function __construct() 
+    {
+        self::setConnection();
+    }
+
+    public static function setConnection($connectionName = "default")
+    {
+        self::$conn = Connection::open($connectionName);
+    }
+
+    public function __set($key, $value)
+    {
+        for($i = 0; $i < count($this->fillable); $i++)
+        {
+            if($key == $this->fillable[$i])
+            {
+                $this->{$key}        = $value;
+                $this->newData[$key] = $value;
+            }
+        }
+    }
+
+    public function __get($key)
+    {
+        return $this->{$key};
+    }
+
+    public function getAll($conditions = [])
+    {
+        $conn = self::$conn;
+
+        $dataQuery = [];
+
+        $where = "WHERE ";
+
+        if(empty($conditions))
+        {
+            $stmt = $conn->query("SELECT * FROM {$this->TABLE}");
+        }
+        else
+        {
+            foreach($conditions as $condition)
+            {
+                $where .= " {$condition[0]} {$condition[1]} :{$condition[0]} " . (isset($condition[3]) ? $condition[3] : "") . " ";
+                $dataQuery += [$condition[0] => $condition[2]];
+            }
+
+            $stmt = $conn->prepare("SELECT * FROM {$this->TABLE} {$where}");
+            $stmt->execute($dataQuery);
+        }
+
+        return $stmt->fetchAll(\PDO::FETCH_CLASS, $this->className);
+    }
+
+    public function find($id)
+    {
+        $conn = self::$conn;
+
+        $stmt = $conn->prepare("SELECT * FROM {$this->TABLE} WHERE id=:id");
+        $stmt->execute(["id" => $id]);
+
+        return $stmt->fetchObject($this->className);
+    }
+
+    public function saveChanges()
+    {
+        $hasNewData = empty($this->newData);
+
+        if($hasNewData)
+        {
+            return;
+        }
+        else
+        {
+            $sqlSetValues = " ";
+            $dataValues["id"] = $this->id;
+
+            foreach($this->newData as $attribute => $value)
+            {
+                $sqlSetValues           .= " {$attribute}=:{$attribute},";
+                $dataValues[$attribute]  = $value;
+            }
+
+            $sqlSetValues = substr($sqlSetValues, 0, -1);
+
+            $sql = "UPDATE {$this->TABLE} SET {$sqlSetValues} WHERE id=:id";
+
+            try
+            {
+                self::$conn->prepare($sql)->execute($dataValues);
+            }
+            catch (\Exception $e)
+            {
+                echo $e->getMessage();
+            }
+        }
+    }
+}
